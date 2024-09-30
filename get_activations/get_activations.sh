d4364055e3667f98301b58654fecd999a2bd29f9
Binh Nguyen <tuanbinhs@gmail.com>
2024-09-30 14:48:04 +0800

    .

--- get_activations/get_activations.sh
+++ get_activations/get_activations.sh
@@ -1,2 +1,5 @@
-python get_activations.py --model_name llama3_8B_instruct --dataset_name tqa_mc2
-python get_activations.py --model_name llama3_8B_instruct --dataset_name tqa_gen_end_q
+
+for MODEL in llama_7B alpaca_7B vicuna_7B llama2_chat_13B llama3_8B; do
+    CUDA_VISIBLE_DEVICES=0 python get_activations.py --model_name $MODEL --dataset_name tqa_mc2
+    CUDA_VISIBLE_DEVICES=0 python get_activations.py --model_name $MODEL --dataset_name tqa_gen_end_q
+done
--- validation/sweeping/ot_intervention.sh
+++ validation/sweeping/ot_intervention.sh
@@ -6,7 +6,7 @@ INFO=ft:davinci-002:ethicalytics:informative:A0WuCDTp
 LOG_FILE="sweep_output.log"
 EVAL_DATASET="truthful_qa"
 TRAIN_DATASET="truthful_qa"
-SAVE="/big_storage/baonn/clf"
+SAVE="/home/users/nus/binhnt/scratch/baonn/clf"
 ##################################################
 #################   Ours   #######################
 ##################################################  
@@ -14,10 +14,10 @@ SAVE="/big_storage/baonn/clf"
 # 'alpaca_7B', 'vicuna_7B', 'llama2_chat_13B', 'llama3_8B'
 # TEST
 for MODEL in llama2_chat_13B; do
-    for alpha in 15 25; do
+    for alpha in 20; do
         for bl in 2.5; do
             echo "model: $MODEL alpha: $alpha bl: $bl"
-            CUDA_VISIBLE_DEVICES=2,3 python ot_edit_layer.py --instruction_prompt default --exp_mode test --clf_only 0 --exp test4 --model_name $MODEL --bl $bl --alpha $alpha --device 0 --num_fold 2 --judge_name $JUDGE --info_name $INFO --eval_dataset $EVAL_DATASET --train_dataset $TRAIN_DATASET --clf_folder $SAVE
+            CUDA_VISIBLE_DEVICES=0 python ot_edit_layer.py --instruction_prompt default --exp_mode test --clf_only 0 --exp test4 --model_name $MODEL --bl $bl --alpha $alpha --device 0 --num_fold 2 --judge_name $JUDGE --info_name $INFO --eval_dataset $EVAL_DATASET --train_dataset $TRAIN_DATASET --clf_folder $SAVE
             echo
             echo
         done
