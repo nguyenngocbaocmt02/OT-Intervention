d3a6a2042753a87af4ab0ccca43ea07a6a43a06b
Bao Nguyen <nguyenngocbaocmt02@gmail.com>
2024-09-30 11:38:16 +0000

    .

---
+++ NQOpen/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nqopen',
+    packages=find_packages(),
+    version='0.0.1',
+)
--- validation/sweeping/sweep_sequential.sh
+++ validation/sweeping/sweep_sequential.sh
@@ -1,8 +1,9 @@
 MODEL=llama_7B
-JUDGE=ft:davinci-002:ethicalytics:truthful:A0WsrZ0l
+#JUDGE=ft:davinci-002:ethicalytics:truthful:A0WsrZ0l # TruthfulQA
+JUDGE=ft:davinci-002:ethicalytics::9t5UpZFx  ## NQOpen
 INFO=ft:davinci-002:ethicalytics:informative:A0WuCDTp
 LOG_FILE="sweep_output.log"
-EVAL_DATASET="truthful_qa"
+EVAL_DATASET="nqopen"
 TRAIN_DATASET="truthful_qa"
 # HF_NAMES = {llama_7B'
 # 'alpaca_7B', 'vicuna_7B', 'llama2_chat_13B', 'llama3_8B'
@@ -13,16 +14,26 @@ TRAIN_DATASET="truthful_qa"
 #TEST
 
 for MODEL in llama_7B; do
-    for alpha in 15; do
-        for K in 48; do
+    for alpha in 0; do
+        for K in 0; do
             echo "model: $MODEL alpha: $alpha K: $K"
-            CUDA_VISIBLE_DEVICES=1 python validate_2fold.py --prompting 50 --instruction_prompt default --model_name $MODEL --use_mode test --num_heads $K --alpha_ot $alpha --device 0 --num_fold 2 --use_center_of_mass --judge_name $JUDGE --info_name $INFO --eval_dataset $EVAL_DATASET --train_dataset $TRAIN_DATASET
+            CUDA_VISIBLE_DEVICES=2 python validate_2fold.py --instruction_prompt default --model_name $MODEL --use_mode test --num_heads $K --alpha_ot $alpha --device 0 --num_fold 2 --use_center_of_mass --judge_name $JUDGE --info_name $INFO --eval_dataset $EVAL_DATASET --train_dataset $TRAIN_DATASET
             echo
             echo
         done
     done
 done
 
+for MODEL in llama_7B; do
+    for alpha in 15; do
+        for K in 48; do
+            echo "model: $MODEL alpha: $alpha K: $K"
+            CUDA_VISIBLE_DEVICES=2 python validate_2fold.py --instruction_prompt default --model_name $MODEL --use_mode test --num_heads $K --alpha_ot $alpha --device 0 --num_fold 2 --use_center_of_mass --judge_name $JUDGE --info_name $INFO --eval_dataset $EVAL_DATASET --train_dataset $TRAIN_DATASET
+            echo
+            echo
+        done
+    done
+done
 ##################################################
 #################   UNINTERVENED    ##############
 ##################################################
