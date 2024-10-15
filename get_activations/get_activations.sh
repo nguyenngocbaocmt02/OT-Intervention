b02a824cee2ffc82391ac848ddf0485e135f8a5a
Bao Nguyen <bao.nn193989@sis.hust.edu.vn>
2024-10-15 15:52:49 +0800

    Update get_activations.sh

--- get_activations/get_activations.sh
+++ get_activations/get_activations.sh
@@ -1,5 +1,5 @@
 
-CACHE="/home/users/nus/binhnt/scratch/.cache/huggingface/hub"
+CACHE=""
 for MODEL in llama2_chat_13B_lofit_fold_0 llama2_chat_13B_lofit_fold_1; do
     CUDA_VISIBLE_DEVICES=0 python get_activations.py --model_name $MODEL --cache_dir $CACHE --dataset_name tqa_mc2
     CUDA_VISIBLE_DEVICES=0 python get_activations.py --model_name $MODEL --cache_dir $CACHE --dataset_name tqa_gen_end_q
